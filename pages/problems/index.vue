311f773be28c9a7eee7c2476546067cfeafa7f0f
drdilyor <drdilyor@outlook.com>
2022-08-26 20:10:16 +0500

    pagination performance

--- pages/problems/index.vue
+++ pages/problems/index.vue
@@ -4,25 +4,40 @@
       v-if="totalPages != -1"
       :cur-page="page"
       :total-pages="totalPages"
-      @set-page="newPage => page = newPage"
+      @set-page="setPage"
       />
-    <api-view :url="'/problems/?page=' + page" ref="view"
-      @done="onApiChange">
-      <template v-slot="{data}">
-        <problem-list :list="data.data"></problem-list>
-      </template>
-    </api-view>
+    <api-view-error v-if="error"></api-view-error>
+    <api-view-pending v-else-if="pending && !data"></api-view-pending>
+    <problem-list v-else :list="data.data"></problem-list>
   </div>
 </template>
 
 <script setup lang="ts">
 import {Ref} from 'vue'
+import {prefix} from '~/api';
+
+interface Paginated {
+  count: number
+  total: number
+  pagesCount: number
+  data: any
+}
 
 const page = ref(1)
 const totalPages = ref(-1)
-const view: Ref<any> = ref(null)
-const onApiChange = ({data}: any) => {
-  totalPages.value = data.pagesCount
+
+const {data, error, pending} = useFetch<Paginated>(
+  () => `${prefix}problems/?page=${page.value}`,
+)
+
+watch(data, () => {
+  if (data.value) {
+    totalPages.value = data.value.pagesCount
+  }
+})
+
+const setPage = (newPage: number) => {
+  page.value = newPage
 }
 
 </script>
