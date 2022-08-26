1d59f47b3a6fa7371fe6f669b08f036f0b9599de
drdilyor <drdilyor@outlook.com>
2022-08-26 05:45:07 +0500

    start

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <button class="button is-primary" @click="increment">{{ count}}</button>
+  </div>
+</template>
+
+<script setup>
+const count = ref(0);
+const increment = () => count.value++;
+</script>
---
+++ components/mdi.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#000000" :d="icons[name]" /> <!-- account -->
+    </svg>
+</template>
+
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+})
+const icons = {
+    home: 'M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z',
+}
+</script>
