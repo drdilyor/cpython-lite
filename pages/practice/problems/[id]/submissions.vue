43f26caa43c601e4cb4cdf35d1492b3550a8ddda
drdilyor <drdilyor@outlook.com>
2022-08-28 20:57:42 +0500

    stub pages

---
+++ pages/practice/problems/[id]/submissions.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="columns">
+    <div class="column is-12">
+      <div class="box">
+        <practice-tabs :problem-id="+$route.params.id"></practice-tabs>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/practice/submit.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="columns">
+    <div class="column is-12">
+      <div class="box">
+        <practice-tabs></practice-tabs>
+      </div>
+    </div>
+  </div>
+</template>
