81daa937136f900a84f2a6e3d1a578cb7d1b023e
drdilyor <drdilyor@outlook.com>
2022-09-01 14:56:09 +0500

    feedback when there are no submissions

--- components/submissions-inner.vue
+++ components/submissions-inner.vue
@@ -1,45 +1,51 @@
 <template>
   <div>
+    <!-- TODO: separate components for short version -->
     <ui-pagination
       v-if="!short"
       :cur-page="page"
       :total-pages="submissions ? submissions.pagesCount : 1"
       @set-page="v => page = v"></ui-pagination>
-    <table v-if="submissions !== null" class="table is-fullwidth is-hoverable">
-      <thead>
-        <th>ID</th>
-        <th>Submitted</th>
-        <th v-if="!short">Language</th>
-        <th v-if="!short">User</th>
-        <th v-if="!short">Problem</th>
-        <th>Verdict</th>
-        <th v-if="!short" class="has-text-right">Time</th>
-        <th v-if="!short" class="has-text-right">Memory</th>
-        <th v-if="!short" class="has-text-right">Code size</th>
-      </thead>
-      <tbody>
-        <tr v-for="s in submissions.data">
-          <td><nuxt-link :to="`/practice/submissions/${s.id}`">
-            {{ s.id }}</nuxt-link></td>
-          <td>
-            <span class="avoid-wrap">{{ new Date(s.created).toLocaleString([], {dateStyle: 'medium'}) }}</span>
-            {{ ' ' }}
-            <span class="avoid-wrap">{{ new Date(s.created).toLocaleString([], {timeStyle: 'short'}) }}</span>
-          </td>
-          <td v-if="!short"><span class="tag">{{ s.lang }}</span></td>
-          <td v-if="!short">{{ s.user.username }}</td>
-          <td v-if="!short"><nuxt-link :to="`/practice/problems/${s.problemId}`">
-            {{ s.problemId }} {{ s.problemTitle }}</nuxt-link></td>
-          <td><span class="tag" :class="verdictColor[s.verdict] ? verdictColor[s.verdict] : 'is-black'">
-            {{ s.verdictTitle }}
-            {{ showTest.includes(s.verdict) ? "#" + s.testCaseNumber : ''}}
-          </span></td>
-          <td v-if="!short" class="has-text-right">{{ s.time }} ms</td>
-          <td v-if="!short" class="has-text-right">{{ s.memory }} KiB</td>
-          <td v-if="!short" class="has-text-right">{{ s.sourceCodeSize }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <template v-if="submissions !== null">
+      <p v-if="submissions.total == 0" class="has-text-centered">
+        No submissions yet.
+      </p>
+      <table v-else class="table is-fullwidth is-hoverable">
+        <thead>
+          <th>ID</th>
+          <th>Submitted</th>
+          <th v-if="!short">Language</th>
+          <th v-if="!short">User</th>
+          <th v-if="!short">Problem</th>
+          <th>Verdict</th>
+          <th v-if="!short" class="has-text-right">Time</th>
+          <th v-if="!short" class="has-text-right">Memory</th>
+          <th v-if="!short" class="has-text-right">Code size</th>
+        </thead>
+        <tbody>
+          <tr v-for="s in submissions.data">
+            <td><nuxt-link :to="`/practice/submissions/${s.id}`">
+              {{ s.id }}</nuxt-link></td>
+            <td>
+              <span class="avoid-wrap">{{ new Date(s.created).toLocaleString([], {dateStyle: 'medium'}) }}</span>
+              {{ ' ' }}
+              <span class="avoid-wrap">{{ new Date(s.created).toLocaleString([], {timeStyle: 'short'}) }}</span>
+            </td>
+            <td v-if="!short"><span class="tag">{{ s.lang }}</span></td>
+            <td v-if="!short">{{ s.user.username }}</td>
+            <td v-if="!short"><nuxt-link :to="`/practice/problems/${s.problemId}`">
+              {{ s.problemId }} {{ s.problemTitle }}</nuxt-link></td>
+            <td><span class="tag" :class="verdictColor[s.verdict] ? verdictColor[s.verdict] : 'is-black'">
+              {{ s.verdictTitle }}
+              {{ showTest.includes(s.verdict) ? "#" + s.testCaseNumber : ''}}
+            </span></td>
+            <td v-if="!short" class="has-text-right">{{ s.time }} ms</td>
+            <td v-if="!short" class="has-text-right">{{ s.memory }} KiB</td>
+            <td v-if="!short" class="has-text-right">{{ s.sourceCodeSize }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
     <api-view-pending v-else-if="pending"></api-view-pending>
     <api-view-error v-else></api-view-error>
   </div>
