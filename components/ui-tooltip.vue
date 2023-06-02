fbb3595e01418ce71eb721dd0233cad35f35bc2a
drdilyor <drdilyor@outlook.com>
2023-06-02 05:26:22 +0500

    add: ui-tooltip

--- components/ui-tooltip.vue
+++ components/ui-tooltip.vue
@@ -1,7 +1,12 @@
 <template>
-  <slot></slot>
+  <span class="group/tooltip">
+    <span class="absolute -mt-5 hidden group-hover/tooltip:inline-block px-1 text-sm rounded bg-black text-white">{{ label }}</span>
+    <slot></slot>
+  </span>
 </template>
 
 <script setup lang="ts">
-
+const props = defineProps({
+  label: { type: String, required: true },
+})
 </script>
