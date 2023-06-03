d1be6cd04befc788ea8fa56f3b0a41c260fd5c1b
drdilyor <drdilyor@outlook.com>
2023-06-03 15:38:23 +0500

    fix: mathjax script is now async

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
       script: [
         {
           src: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS-MML_HTMLorMML&delayStartupUntil=configured',
+          async: true,
         },
       ],
     },
