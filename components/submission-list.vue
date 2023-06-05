7f0ea5170cce08fc9aa5df7da315b5ca6437963f
drdilyor <drdilyor@outlook.com>
2023-06-05 09:40:29 +0500

    fix: submission list table width

--- components/submission-list.vue
+++ components/submission-list.vue
@@ -2,13 +2,15 @@
   <ui-table v-slot="{table, thead, th, tr, td}">
     <table :class="table" class="min-w-[50rem]">
       <thead :class="thead">
-        <th :class="th" class="w-20">ID</th>
-        <th :class="th">When</th>
-        <th :class="th" class="w-48 lg:w-64">Who</th>
-        <th :class="th" class="w-32">Lang</th>
-        <th :class="th" class="w-20">Verdict</th>
-        <th :class="th" class="w-24">Time</th>
-        <th :class="th" class="w-24">Memory</th>
+        <tr>
+          <th :class="th" class="w-20">ID</th>
+          <th :class="th" class="w-28 lg:w-48">When</th>
+          <th :class="th" class="w-48 lg:w-auto">Who</th>
+          <th :class="th" class="w-32">Lang</th>
+          <th :class="th" class="w-20">Verdict</th>
+          <th :class="th" class="w-24 text-right">Time</th>
+          <th :class="th" class="w-24 text-right">Memory</th>
+        </tr>
       </thead>
       <tbody>
         <tr v-for="submission in submissions" :class="tr">
@@ -19,7 +21,7 @@
             <template v-else>{{ submission.id }}</template>
           </td>
           <td :class="td"><ui-time :value="submission.created" date time seconds></ui-time></td>
-          <td :class="td"><ui-user :user="submission.user" limit-width class="w-44 lg:w-60"></ui-user></td>
+          <td :class="td"><ui-user :user="submission.user" limit-width class="w-44 lg:w-auto"></ui-user></td>
           <td :class="td"><ui-language :lang="submission.lang"></ui-language></td>
           <td :class="td"><submission-verdict :submission="submission"></submission-verdict></td>
           <td :class="td" class="text-right">{{ submission.time }}ms</td>
