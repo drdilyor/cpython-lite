a6328c45ecd10733a6aa54a0ed7c0ec9da1aeafd
drdilyor <drdilyor@outlook.com>
2023-06-05 21:40:51 +0500

    hack: hide some navbar elements on small screens for responsiveness for *now*

--- components/app-navbar.vue
+++ components/app-navbar.vue
@@ -2,8 +2,8 @@
   <div class="flex items-stretch px-4 h-12 shadow-md">
     <nuxt-link class="flex items-center px-2 hover:bg-gray-200" to="/">
       <img class="h-8 w-8" src="@/assets/logo.png" alt="CPython lite logo">
-      <span class="text-primary-500">CPython</span>
-      <span class="text-secondary-500 relative bottom-1">Lite</span>
+      <span class="hidden sm:inline text-primary-500">CPython</span>
+      <span class="hidden sm:inline relative bottom-1 text-secondary-500">Lite</span>
     </nuxt-link>
     <nuxt-link class="flex items-center px-2 hover:bg-gray-200" to="/problems">Problems</nuxt-link>
     <nuxt-link class="flex items-center px-2 hover:bg-gray-200" to="/contests">Contests</nuxt-link>
@@ -12,18 +12,18 @@
       <div class="h-full flex items-center px-2 group-hover:bg-gray-200">
         <template v-if="user.pending">
           <div class="w-8 h-8 mr-2 bg-black opacity-10 rounded-full"></div>
-          <div class="w-16 h-4 bg-black opacity-10 rounded"></div>
+          <div class="hidden sm:block w-16 h-4 bg-black opacity-10 rounded"></div>
         </template>
         <template v-else-if="user.user">
-          <img class="w-8 h-8 mr-2 rounded-full" :src="user.user.avatar" alt="User avatar">
-          <div>{{ user.user.username }}</div>
+          <img class="w-8 h-8 rounded-full" :src="user.user.avatar" alt="User avatar">
+          <div class="hidden sm:block ml-2">{{ user.user.username }}</div>
         </template>
         <template v-else>
           <ui-button url="/login">Login</ui-button>
         </template>
       </div>
       <template v-if="user.user">
-        <div class="absolute invisible group-hover:visible flex flex-col items-stretch w-full bg-white shadow-md">
+        <div class="absolute invisible right-0 min-w-full group-hover:visible flex flex-col items-stretch bg-white shadow-md">
           <div class="px-2 py-2 hover:bg-gray-200 cursor-pointer" @click="logoutUser">Logout</div>
         </div>
       </template>
