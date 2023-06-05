f044a30215e22cbd618d84a82568d1b30db5cb53
drdilyor <drdilyor@outlook.com>
2023-06-05 09:37:32 +0500

    add: submission detail

--- components/submission-list.vue
+++ components/submission-list.vue
@@ -1,31 +1,38 @@
 <template>
-  <table class="w-full">
-    <thead class="bg-primary-600 text-white">
-      <td class="p-2">ID</td>
-      <td class="p-2">When</td>
-      <td class="p-2">Who</td>
-      <td class="p-2">Lang</td>
-      <td class="p-2">Verdict</td>
-      <td class="p-2">Time</td>
-      <td class="p-2">Memory</td>
-    </thead>
-    <tbody>
-      <tr v-for="submission in submissions">
-        <td class="p-2">
-          <nuxt-link :to="`/submissions/${submission.id}`">{{ submission.id }}</nuxt-link>
-        </td>
-        <td class="p-2"><ui-time :value="submission.created" date time seconds></ui-time></td>
-        <td class="p-2"><ui-user :user="submission.user"></ui-user></td>
-        <td class="p-2"><ui-language :lang="submission.lang"></ui-language></td>
-        <td class="p-2"><submission-verdict :submission="submission"></submission-verdict></td>
-        <td class="p-2 text-right">{{ submission.time }}ms</td>
-        <td class="p-2 text-right">{{ submission.memory }}KB</td>
-      </tr>
-    </tbody>
-  </table>
+  <ui-table v-slot="{table, thead, th, tr, td}">
+    <table :class="table" class="min-w-[50rem]">
+      <thead :class="thead">
+        <th :class="th" class="w-20">ID</th>
+        <th :class="th">When</th>
+        <th :class="th" class="w-48 lg:w-64">Who</th>
+        <th :class="th" class="w-32">Lang</th>
+        <th :class="th" class="w-20">Verdict</th>
+        <th :class="th" class="w-24">Time</th>
+        <th :class="th" class="w-24">Memory</th>
+      </thead>
+      <tbody>
+        <tr v-for="submission in submissions" :class="tr">
+          <td :class="td">
+            <nuxt-link v-if="user.user.is_superuser || user.user.username == submission.user.username" :to="`/submissions/${submission.id}`">
+              {{ submission.id }}
+            </nuxt-link>
+            <template v-else>{{ submission.id }}</template>
+          </td>
+          <td :class="td"><ui-time :value="submission.created" date time seconds></ui-time></td>
+          <td :class="td"><ui-user :user="submission.user" limit-width class="w-44 lg:w-60"></ui-user></td>
+          <td :class="td"><ui-language :lang="submission.lang"></ui-language></td>
+          <td :class="td"><submission-verdict :submission="submission"></submission-verdict></td>
+          <td :class="td" class="text-right">{{ submission.time }}ms</td>
+          <td :class="td" class="text-right">{{ submission.memory }}KB</td>
+        </tr>
+      </tbody>
+    </table>
+  </ui-table>
 </template>
 <script setup lang="ts">
 defineProps({
   submissions: { type: Array<any>, required: true },
 })
+
+const user = useUser()
 </script>
---
+++ pages/submissions/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="p-4">
+    <error-loading-view v-bind="{ pending, error, refresh}">
+      <submission-list :submissions="[submission]"></submission-list>
+      <pre class="p-2 font-mono bg-gray-50">{{ submission.sourceCode }}</pre>
+    </error-loading-view>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const {data: submission, pending, error, refresh} = useAsyncData(
+  () =>$get(`/attempts/${+route.params.id}`),
+  {lazy: true},
+)
+</script>
