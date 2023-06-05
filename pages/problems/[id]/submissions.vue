9166b29f9d98807905d35df9c7e3070c4b381ee5
drdilyor <drdilyor@outlook.com>
2023-06-05 09:44:51 +0500

    add: pagination to problem submissions

--- pages/problems/[id]/submissions.vue
+++ pages/problems/[id]/submissions.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="p-4">
     <problem-tabs :id="+route.params.id"></problem-tabs>
+    <h1 v-if="problem && problem.id == +route.params.id" class="text-4xl my-4">{{ problem.id }}. {{ problem.title }}</h1>
+    <ui-pagination v-if="submissions" :cur-page="page" :total-pages="submissions.pagesCount" @set-page="value => page = value"></ui-pagination>
     <error-loading-view :pending="pending || problemPending" :error="error || problemError" :refresh="() =>(refresh(), problemRefresh())">
-      <h1 class="text-4xl my-4">{{ problem.id }}. {{ problem.title }}</h1>
       <submission-list :submissions="submissions.data"></submission-list>
     </error-loading-view>
   </div>
@@ -11,13 +12,20 @@
 <script setup lang="ts">
 const route = useRoute()
 
+const page = ref(1)
+
 const {data: submissions, pending, error, refresh} = useAsyncData(
   () => $get<any>(`/attempts`, {
     query: {
       problem_id: route.params.id,
+      page_size: 20,
+      page: page.value,
     }
   }),
-  {lazy: true}
+  {
+    lazy: true,
+    watch: [page],
+  }
 )
 
 const {data: problem, pending: problemPending, error: problemError, refresh: problemRefresh} = useAsyncData(
