85dd73ac3f8fd91d44a190b64818544ed144ff4b
drdilyor <drdilyor@outlook.com>
2023-06-06 07:42:37 +0500

    add: all submissions checkbox

--- components/ui-tabs.vue
+++ components/ui-tabs.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="flex mb-4">
-    <nuxt-link v-for="tab in tabs" :to="tab.url"
-      class="p-2 border-b-2 hover:bg-gray-200"
-      :class="[route.path == tab.url ? 'color-primary-700 border-b-primary-600' : 'border-b-transparent']">
-      {{ tab.text }}
-    </nuxt-link>
+    <template v-if="style == 'underline'">
+      <nuxt-link v-for="tab in tabs" :to="tab.url"
+        class="p-2 border-b-2 hover:bg-gray-200"
+        :class="[route.path == tab.url ? 'color-primary-700 border-b-primary-600' : 'border-b-transparent']">
+        {{ tab.text }}
+      </nuxt-link>
+    </template>
+    <template v-else-if="style == 'buttoned'">
+      <ui-button v-for="tab in tabs" :to="tab.url"
+        :light="route.path == tab.url">
+        {{ tab.text }}
+      </ui-button>
+    </template>
   </div>
 </template>
 
@@ -17,6 +25,7 @@ interface Tab {
 
 const props = defineProps({
   tabs: { type: Array<Tab>, required: true},
+  style: { type: String as PropType<'underline' | 'buttoned'>, default: 'underline'},
 })
 
 const route = useRoute()
--- pages/contests/[id]/submissions/index.vue
+++ pages/contests/[id]/submissions/index.vue
@@ -4,6 +4,10 @@
     <error-loading-view :pending="contest.pending || pending" :error="contest.error || error"
       :refresh="() => (refresh(), refreshContest())">
       <h1 class="text-4xl my-4">{{ contest.info.title }}</h1>
+      <div class="flex items-center my-4">
+        <ui-checkbox id="all-submissions" v-model="all" class="mr-2"></ui-checkbox>
+        <label for="all-submissions">All submissions</label>
+      </div>
       <submission-list v-if="submissions.total" :submissions="submissions.data"></submission-list>
       <p v-else>No submissions.</p>
     </error-loading-view>
@@ -17,16 +21,18 @@ const user = useUser()
 const contest = useContest()
 fetchContest(+route.params.id)
 
+const all = ref(false)
+
 const { data: submissions, pending, error, refresh } = useAsyncData(
   `/attempts?contest_id=${+route.params.id}`,
   () => $get<any>(`/attempts`, {
     query: {
       contest_id: +route.params.id,
       page_size: 100,
-      username: user.user ? user.user.username : undefined,
+      username: !all.value && user.user ? user.user.username : undefined,
     },
   }),
-  { lazy: true },
+  { lazy: true, watch: [all] },
 )
 
 </script>
