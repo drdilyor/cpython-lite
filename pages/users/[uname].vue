cab797c3acb4b0e84dc730023711837ef57a8b71
drdilyor <drdilyor@outlook.com>
2023-06-06 11:42:38 +0500

    fix: image layout shift in profile and improve users without names

--- pages/users/[uname].vue
+++ pages/users/[uname].vue
@@ -1,18 +1,23 @@
 <template>
   <div v-if="user && !error" class="p-4">
     <div>
-      <div class="max-h-96 overflow-hidden -mx-4 transition-all">
-        <img :src="user.coverPhoto" alt="">
+      <div class="max-h-96 overflow-hidden -mx-4 transition-all" :class="!coverLoaded ? 'h-96' : ''">
+        <img :src="user.coverPhoto" alt="User banner" @onload="coverLoaded = true">
       </div>
-      <div class="relative flex min-h-20 pb-4 md:px-16">
-        <img class="w-36 md:w-52 shadow-lg bg-white border-4 border-white absolute bottom-4" :src="user.avatar" alt="">
+      <div class="relative flex pb-4 md:px-16">
+        <img class="w-36 md:w-52 shadow-lg bg-white border-4 border-white absolute bottom-4" :src="user.avatar" alt="User avatar">
         <div class="w-36 md:w-52 mr-4 shrink-0">
         </div>
         <div class="pt-2">
           <div class="text-xl">
-            {{ user.firstName || '' }} {{ user.lastName || '' }}
+            <template v-if="user.firstName || user.lastName">
+              {{ user.firstName || '' }} {{ user.lastName || '' }}
+            </template>
+            <template v-else>
+              {{ user.username }}
+            </template>
           </div>
-          <div class="text-gray-600">{{ user.username }}</div>
+          <div class="text-gray-600" v-if="user.firstName || user.lastName">{{ user.username }}</div>
         </div>
       </div>
     </div>
@@ -34,4 +39,6 @@ const tabs = [
   {url: `/users/${route.params.uname}`, text: 'Ratings'},
   {url: `/users/${route.params.uname}/blog`, text: 'Blog'},
 ]
+
+const coverLoaded = ref(false)
 </script>
