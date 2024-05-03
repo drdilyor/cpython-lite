3244f3ec91e7fd92e5632094e7d6183ef6e8a32a
drdilyor <drdilyor@outlook.com>
2024-05-03 18:32:43 +0500

    fix

--- components/ui-input-label.vue
+++ components/ui-input-label.vue
@@ -2,8 +2,7 @@
   <label :class="[
     breakpoint == 'sm' ? 'sm:text-right sm:py-1 sm:pr-2' :
     breakpoint == 'md' ? 'md:text-right md:py-1 md:pr-2' :
-    breakpoint == 'lg' ? 'lg:text-right lg:py-1 lg:pr-2' : '',
-    'w-32 mb-2']">
+    breakpoint == 'lg' ? 'lg:text-right lg:py-1 lg:pr-2' : '']">
     <slot></slot>
   </label>
 </template>
